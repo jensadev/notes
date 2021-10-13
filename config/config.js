4d7c28894749ca2e1cabdc3475693981597e305e
jensnti <jensandreasson77@gmail.com>
2021-10-13 18:55:54 +0200

    🔧 config

--- config/config.js
+++ config/config.js
@@ -1,16 +1,21 @@
+require('dotenv').config();
+
 module.exports = {
     development: {
         storage: 'database.sqlite',
         dialect: 'sqlite'
     },
     test: {
-        storage: 'database.sqlite',
-        dialect: 'sqlite'
+        username: process.env.DB_USER || '',
+        password: process.env.DB_PASSWORD || '',
+        database: process.env.DB_NAME || '',
+        host: process.env.DB_HOST || '',
+        dialect: 'postgres'
     },
     production: {
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
+        database: process.env.DB_NAME,
         host: process.env.DB_HOST,
         dialect: 'mysql'
     }
