5ef48ed63527b4383df8434f5f509b37eccdbedf
Jens Andreasson <jens.andreasson@ga.ntig.se>
2021-10-14 11:57:52 +0200

    🔧

--- config/config.js
+++ config/config.js
@@ -6,17 +6,14 @@ module.exports = {
         dialect: 'sqlite'
     },
     test: {
+        storage: 'database.test.sqlite',
+        dialect: 'sqlite'
+    },
+    production: {
         username: process.env.DB_USER || '',
         password: process.env.DB_PASSWORD || '',
         database: process.env.DB_NAME || '',
         host: process.env.DB_HOST || '',
         dialect: 'postgres'
-    },
-    production: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        dialect: 'mysql'
     }
 };
